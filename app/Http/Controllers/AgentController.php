1829ee7c499751b3f3d98364aa5cc0c4417015d2
Mergen Nuryyev <m.nuryyev@iqfulfillment.com>
2023-07-16 16:18:11 +0400

    updates

---
+++ app/Http/Controllers/AgentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AgentController extends Controller
+{
+    public function agentDashboard(){
+        return view('agent.agent');
+    }
+}
---
+++ resources/views/agent/agent.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compat" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Agent Dashboard</title>
+</head>
+<body>
+<h1> Agent Dashboard </h1>
+
+</body>
+</html>
