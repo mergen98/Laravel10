ef3c428491f33bca2d91c254443bc4bb75697b7a
Mergen Nuryyev <m.nuryyev@iqfulfillment.com>
2023-07-20 00:10:12 +0400

    admin login and logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class AdminController extends Controller
 {
     public function adminDashboard(){
         return view('admin.index');
+    }
+    
+    public function adminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        
+        $request->session()->invalidate();
+        
+        $request->session()->regenerateToken();
+        
+        return redirect('/admin/login');
+    }
+    public function adminLogin(){
+        return view('admin.admin_login');
     }
 
 }
